7bbad46b04277a3d43efbf57ae8af1aafbaa4aa8
Antony <antony.caiquec@gmail.com>
2023-12-15 10:44:51 -0300

    Alteração de estilo dos icones

--- resources/js/Components/NavBar/NavBar.logic.tsx
+++ resources/js/Components/NavBar/NavBar.logic.tsx
@@ -11,8 +11,8 @@ interface NavItem {
 }
 
 export const navItems: NavItem[] = [
-    { page: "/", icon: <FaHome color="#5CC6BA" />, label: "Home" },
-    { page: "/booking", icon: <FaCalendar color="#5CC6BA" />, label: "Booking" },
-    { page: "/account", icon: <FaUser color="#5CC6BA" />, label: "Account" },
+    { page: "/", icon: <FaHome color="#5CC6BA" className='w-8 mb-1' />, label: "Home" },
+    { page: "/booking", icon: <FaCalendar color="#5CC6BA" className='w-8 mb-1' />, label: "Booking" },
+    { page: "/account", icon: <FaUser color="#5CC6BA" className='w-8 mb-1' />, label: "Account" },
   ];
 
