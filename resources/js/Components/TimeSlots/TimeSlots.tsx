90fee5f53ae5bd8b7573fcd64f8208d8087cd36e
Antony <antony.caiquec@gmail.com>
2023-12-19 11:38:42 -0300

    Adição de tabs respectivas para os horarios

--- resources/js/Components/TimeSlots/TimeSlots.tsx
+++ resources/js/Components/TimeSlots/TimeSlots.tsx
@@ -1,69 +1,63 @@
-    import React from 'react';
-    import { Box, Flex, Text } from '@chakra-ui/react';
-    import Button from "@/Components/Button/Button";
-    import { times } from './app';
-    import './TimeSlots.css'
+import React from 'react';
+import { Box, Flex, Text, Tab, TabList, TabPanel, TabPanels, Tabs, TabIndicator } from '@chakra-ui/react';
+import { times } from './app';
+import './TimeSlots.css';
+import { useTimeSelection } from './app';
 
-    interface TimeSlotsProps {
-        onSelectTime: (selectedTime: string | null) => void;
-        unavailableTimes?: string[];
-    }
+interface TimeSlotsProps {
+    onSelectTime: (selectedTime: string | null) => void;
+    unavailableTimes?: string[];
+}
 
+const TimeSlots = ({ onSelectTime, unavailableTimes = [] }: TimeSlotsProps) => {
 
-    const TimeSlots = ({ onSelectTime, unavailableTimes = [] }: TimeSlotsProps) => {
-        const [selectedTime, setSelectedTime] = React.useState<string | null>(null);
+    const {selectedTime, handleTimeClick, setSelectedTime} = useTimeSelection(onSelectTime);
 
-        const handleTimeClick = (time: string) => {
-            setSelectedTime(time === selectedTime ? null : time);
-            onSelectTime(time === selectedTime ? null : time);
-        };
+    return (
+        <Box className="TimeSlots">
+            <Tabs isLazy variant={'soft-rounded'} align='center' onChange={() => setSelectedTime(null)}>
+                <TabList mb={1}>
+                    {times.map((period) => (
+                        <Tab key={period.label} _selected={{ bg: '#5CC6BA', color: '#fff' }}>{period.label}</Tab>
+                    ))}
+                </TabList>
+                <TabPanels>
+                    {times.map((period, periodIndex) => (
+                        <TabPanel key={periodIndex}>
+                            <Flex justify="center" flexWrap="wrap" gap={3}>
+                                {period.hours.map((time, index) => {
+                                    const isUnavailable = unavailableTimes.includes(time);
+                                    return (
+                                        <Box
+                                            aria-disabled={isUnavailable}
+                                            key={index}
+                                            textAlign="center"
+                                            borderWidth="1px"
+                                            borderRadius="md"
+                                            w={'25%'}
+                                            m={1.5}
+                                            color={selectedTime === time ? '#FFFFFF' : '#A0A0A0'}
+                                            backgroundColor={selectedTime === time ? '#5CC6BA' : 'transparent'}
+                                            _hover={{ cursor: 'pointer' }}
+                                            onClick={!isUnavailable ? () => handleTimeClick(time) : undefined}
+                                            transition="background-color 0.2s ease-in"
+                                            _disabled={{
+                                                color: '#CCCCCC',
+                                                cursor: 'not-allowed',
+                                                backgroundColor: '#F0F0F0',
+                                            }}
+                                        >
+                                            {time}
+                                        </Box>
+                                    );
+                                })}
+                            </Flex>
+                        </TabPanel>
+                    ))}
+                </TabPanels>
+            </Tabs>
+        </Box>
+    );
+};
 
-        return (
-            <Box className="TimeSlots">
-                {times.map((period, periodIndex) => (
-                    <React.Fragment key={periodIndex}>
-                        <Text my={'2'}>
-                            {period.label}
-                        </Text>
-                        <Flex
-                            justify="center"
-                            flexWrap="wrap"
-                        >
-                            {period.hours.map((time, index) => {
-                                const isUnavailable = unavailableTimes.includes(time);
-                                return (
-                                    <Box
-                                        aria-disabled={isUnavailable}
-                                        key={index}
-                                        textAlign="center"
-                                        borderWidth="1px"
-                                        borderRadius="md"
-                                        w={20}
-                                        m={1.5}
-                                        color={selectedTime === time ? '#FFFFFF' : '#A0A0A0'}
-                                        backgroundColor={selectedTime === time ? '#5CC6BA' : 'transparent'}
-                                        _hover={{ cursor: 'pointer' }}
-                                        onClick={!isUnavailable ? () => handleTimeClick(time) : undefined}
-                                        transition="background-color 0.2s ease-in"
-                                        _disabled={{
-                                            color: '#CCCCCC',
-                                            cursor: 'not-allowed',
-                                            backgroundColor: '#F0F0F0'
-                                        }}
-                                    >
-                                        {time}
-                                    </Box>
-                                );
-                            })}
-                        </Flex>
-                    </React.Fragment>
-                ))}
-                <Text m={5} textAlign="center">
-                    Hora selecionada: {selectedTime || 'Nenhum'}
-                </Text>
-            </Box>
-        );
-    };
-
-    export default TimeSlots;
-
+export default TimeSlots;
