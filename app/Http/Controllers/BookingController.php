af7bcf04bbcea3944fbfc20ff038e805420436d2
Antony <antony.caiquec@gmail.com>
2023-12-21 13:01:31 -0300

    Criação da rota e método de exclusão de Booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -11,4 +11,17 @@ class BookingController extends Controller
         $booking = Booking::where('date', $req->input('date') )->pluck('time')->toArray();
         return response()->json(['unavaiableTimes' => $booking]);
     }
+
+    public function destroy(int $id)
+    {
+        try {
+            $booking = Booking::findOrFail($id);
+
+            $booking->delete();
+
+            return response()->json(['message' => 'Agendamento excluído com sucesso!'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error deleting booking', 'message' => $e->getMessage()], 500);
+        }
+    }
 }
