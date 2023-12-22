1dba2416560dd3940150a1129509185d0ef39726
Antony <antony.caiquec@gmail.com>
2023-12-22 10:30:33 -0300

    Remoção da prop "visibility" que controlava o display do Button

--- resources/js/Components/Button/Button.tsx
+++ resources/js/Components/Button/Button.tsx
@@ -6,14 +6,13 @@ interface ButtonProps {
     width?: string|number,
     isLoading?: boolean
     as?: As
-    visible?: boolean,
     onClick?: ({param} : any) => void | Promise<void>;
 }
-const Button = ({ bg="#5CC6BA", width=200, children, visible=true,isLoading = false, as = undefined, onClick}: ButtonProps) => {
+const Button = ({ bg="#5CC6BA", width=200, children,isLoading = false, as = undefined, onClick}: ButtonProps) => {
     return (
         <>
             <Flex justifyContent={'center'}>
-                <ButtonUI variant={'custom'} _hover={{opacity:.6}} className="default-button" bg={bg} color='#fff' width={width} isLoading={isLoading} as={as} onClick={onClick} display={visible ? '' : 'none'}>
+                <ButtonUI variant={'custom'} _hover={{opacity:.6}} className="default-button" bg={bg} color='#fff' width={width} isLoading={isLoading} as={as} onClick={onClick}>
                     {children}
                 </ButtonUI>
             </Flex>
