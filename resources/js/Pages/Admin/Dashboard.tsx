4e8a649e11d0d31c1dc743d0200fefa04d510105
Antony <antony.caiquec@gmail.com>
2024-01-08 10:59:01 -0300

    Manutenção das tabs

--- resources/js/Pages/Admin/Dashboard.tsx
+++ resources/js/Pages/Admin/Dashboard.tsx
@@ -1,48 +1,60 @@
-import React, { useState } from 'react';
-import { Box, Flex } from "@chakra-ui/react";
+import { TabList, Tabs, Tab, TabPanel, TabPanels } from "@chakra-ui/react";
 import Header from '@/Components/Header/Header';
 import NavAdmin from '@/Components/NavAdmin/NavAdmin';
 import PageLoading from '@/Components/PageLoading/PageLoading';
+import RevenueDatePicker from '@/Components/RevenueDatePicker/RevenueDatePicker';
+import Bookings from "./ManagerBookings";
+import BookingForm from "@/Components/BookingForm/BookingForm";
+import UserList from "@/Components/UserList/UserList";
+import { useEffect, useState } from "react";
 
-const ButtonAdmin = ({ children, onClick, isActive }: any) => {
-  return (
-    <Box
-      as="button"
-      borderWidth={1}
-      borderRadius={10}
-      fontWeight={'bold'}
-      fontSize={16}
-      width={150}
-      textAlign={'center'}
-      backgroundColor={isActive ? '#5CC6BA' : 'transparent'}
-      color={isActive ? '#ECECEC' : '#A0A0A0'}
-      outline="none"
-      onClick={onClick}
-    >
-      {children}
-    </Box>
-  );
-};
+const Dashboard = ({ services, users }: any) => {
+    useEffect(() => {
+        console.dir(users)
+    }, [])
 
-const Dashboard = () => {
-  const [activeButton, setActiveButton] = useState<string | null>(null);
+    const [id, setId] = useState<number | undefined>();
+    const [activeTab, setActiveTab] = useState<number>(0);
+    const onBookingFor = (id: number) => {
+        setId(id);
+        setActiveTab(1);
+    }
+    return (
+        <>
+            <Header text="Admin" />
+            <Tabs defaultIndex={0} index={activeTab} isLazy variant={'soft-rounded'} align='center' onChange={(index) => setActiveTab(index)}>
+                <TabList mb={5} gap={0} >
 
-  const handleButtonClick = (buttonName: string) => {
-    setActiveButton(buttonName === activeButton ? null : buttonName);
-  };
+                    <Tab _selected={{ bg: '#5CC6BA', color: '#fff' }}>
+                        Clientes
+                    </Tab>
+                    <Tab _selected={{ bg: '#5CC6BA', color: '#fff' }}>
+                        Agendar
+                    </Tab>
+                    <Tab _selected={{ bg: '#5CC6BA', color: '#fff' }}>
+                        Faturamento
+                    </Tab>
+                </TabList>
 
-  return (
-    <>
-      <Header text="Admin" />
-      <Flex flexWrap={'wrap'} gap={5} justifyContent={'center'} margin={'200px auto'}>
-        <ButtonAdmin onClick={() => handleButtonClick('Agendar')} isActive={activeButton === 'Agendar'}>Agendar</ButtonAdmin>
-        <ButtonAdmin onClick={() => handleButtonClick('Definir Horário')} isActive={activeButton === 'Definir Horário'}>Definir Horário</ButtonAdmin>
-        <ButtonAdmin onClick={() => handleButtonClick('Faturamento')} isActive={activeButton === 'Faturamento'}>Faturamento</ButtonAdmin>
-      </Flex>
-      <NavAdmin activePage="Dashboard" />
-      <PageLoading />
-    </>
-  );
+                <TabPanels>
+
+                    <TabPanel>
+                        <UserList onUserSelect={onBookingFor} users={users} />
+                    </TabPanel>
+                    <TabPanel>
+                        <BookingForm id={id} services={services} formNeed></BookingForm>
+                    </TabPanel>
+                    <TabPanel>
+                        <RevenueDatePicker />
+
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+
+            <NavAdmin activePage="Dashboard" />
+            <PageLoading />
+        </>
+    );
 };
 
 export default Dashboard;
