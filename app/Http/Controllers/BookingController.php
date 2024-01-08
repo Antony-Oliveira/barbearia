cb8b087eb003867c7584e6ff4ee7472528cc6605
Antony <antony.caiquec@gmail.com>
2024-01-08 10:45:41 -0300

    fix

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Booking;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -34,5 +35,25 @@ class BookingController extends Controller
             return response()->json(["error" => 'Erro update booking!', 'message' => $e->getMessage()], 500);
         }
 
+    }
+
+    public function create(Request $req) {
+        try {
+            $user = auth()->user();
+            $services = Service::whereIn('name', $req->input('services'))->get();
+            $totalPrice = $services->sum('price');
+            $booking = Booking::create([
+                'user_id' => $user->id,
+                'date' => $req->input('date'),
+                'time' => $req->input('time'),
+                'total_price' => $totalPrice,
+            ]);
+
+            $booking->services()->attach($services);
+            return response()->json(['r' => 'ok']);
+        } catch (\Exception $e) {
+            return response()->json(['N' => $e->getMessage()]);
+
+        }
     }
 }
